b65a86c7a678f43a5941c02a660b766546231e83
chenlong <951709483@qq.com>
2021-02-26 16:01:43 +0800

    Initial commit

---
+++ parent/manager/web/src/main/java/com/cl/Application.java
@@ -0,0 +1,16 @@
+package com.cl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description 启动类
+ * @auther chenlong
+ * @date 2021/2/2615:37
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
