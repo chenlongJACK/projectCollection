91e81f04b675a92109f946257060c369d739e059
chenlong <951709483@qq.com>
2021-04-14 16:24:51 +0800

    提交

--- parent/manager/web/src/main/java/com/cl/controller/AccountController.java
+++ parent/manager/web/src/main/java/com/cl/controller/AccountController.java
@@ -3,12 +3,19 @@ package com.cl.controller;
 
 import com.cl.bean.Account;
 import com.cl.common.bean.ResultInfo;
+import com.cl.dao.IAccountDao;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * @Description
@@ -20,9 +27,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(description = "用户管理")
 public class AccountController {
 
+    private static Logger logger=LoggerFactory.getLogger(AccountController.class);
+    @Autowired
+    private IAccountDao accountDao;
+
     @PostMapping("/login")
     @ApiOperation(value = "登录")
     public ResultInfo login(@Validated Account account){
+        account.setId(UUID.randomUUID().toString().replaceAll("-",""));
+        accountDao.insert(account);
         ResultInfo resultInfo=new ResultInfo();
         return resultInfo;
     }
--- parent/manager/web/src/test/java/com/cl/test/ApplicationTest.java
+++ parent/manager/web/src/test/java/com/cl/test/ApplicationTest.java
@@ -2,6 +2,7 @@ package com.cl.test;
 
 import com.cl.bean.Account;
 import com.cl.dao.IAccountDao;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +27,6 @@ public class ApplicationTest {
         String uuid = UUID.randomUUID().toString();
         String id = uuid.replaceAll("-", "");
         int insert = accountDao.insert(new Account(id, "admin", "123456", "admin"));
-        if(insert>0){
-            System.out.println("插入成功");
-        }else{
-            System.out.println("插入失败");
-        }
+        Assert.assertEquals(2,insert);
     }
 }
